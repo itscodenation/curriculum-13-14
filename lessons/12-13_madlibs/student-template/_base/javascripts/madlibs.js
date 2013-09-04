42e3a236d7968cb6dc42073b18d906ba87c92602
Gracie Elqura <gracie.elqura@gmail.com>
2013-09-04 11:56:34 -0400

    Add madlibs

---
+++ lessons/12-13_madlibs/student-template/_base/javascripts/madlibs.js
@@ -0,0 +1,39 @@
+
+
+$(document).ready(function() {
+
+	//This is the story with placeholder words
+	var story = "It has often been said that 'a dog is a man's best NOUN_1.' " +
+	"Dogs are very ADJECTIVE_1 and can be taught many ADJECTIVE_2 tricks. " +
+	"A dog can be trained to carry a/an NOUN_2 in his mouth. " + 
+	"And if you throw this NOUN_3, he will run and fetch it. " + 
+	"Dogs will also bark ADVERB_1 if someone tries to break into your NOUN_4 " + 
+	"during the night. One of the most popular canine pets today is the NOUN_5 " +
+	"Spaniel. Spaniels have curly COLOR coats and ADJECTIVE_3 ears. " +
+	"They also have very ADJECTIVE_4 dispositions and live to be NUMBER years old. "+
+	"Other popular dogs are ADJECTIVE_5 Terriers, German PLURAL_NOUN, and " +
+	"the ADJECTIVE_6 Poodle. Every home should have a loyal dog for a/an NOUN_6.";
+
+	//display to the console
+	console.log(story);
+	
+	//Prompt the user for each of the words and then store them
+	
+	
+	//Replace all placeholders words in the story
+	
+
+	//Display the final story with the replaced words by calling the displayStory function
+	
+	
+
+	//Function that displays the final story in the HTML
+	function displayStory(){
+		//This displays the final story
+		//Don't worry about how this works....yet :) 
+		$("#story").text(story);	
+	}
+		
+});
+
+
