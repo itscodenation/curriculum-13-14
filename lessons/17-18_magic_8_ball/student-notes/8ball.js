52f0a6da8be56ddeca3d0b595d9e47b30d304d2f
elju <moresilenter@gmail.com>
2014-04-23 13:59:09 -0400

    quick changes to student notes for magic 8ball

--- lessons/17-18_magic_8_ball/student-notes/8ball.js
+++ lessons/17-18_magic_8_ball/student-notes/8ball.js
@@ -1,3 +1,11 @@
 function getFortune() {
   // Fill in your code here!
 }
+
+// Use this function once you have your text
+// to place it on the page. For instance,
+// fillInAnswer("THIS IS JUST RANDOM TEXT")
+// will write that on the page!
+function fillInAnswer(str) {
+  $('#answer').text(str);
+}
